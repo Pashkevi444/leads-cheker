4d486b6099d690e5edd62a9d88987b58b923e77a
paul.balagansky <pasha.balaganskii@gmail.com>
2024-12-19 19:47:57 +0700

    CHORE tiny fixes 4

---
+++ paul.leadschecker/lang/en/install/step.php
@@ -0,0 +1,5 @@
+<?php
+
+    $MESS["PAUL_LEADCHECKER_SETTINGS_STEP_BEFORE"]        = "Paul Module";
+    $MESS["PAUL_LEADCHECKER_SETTINGS_STEP_AFTER"]         = "Has been installed";
+    $MESS["PAUL_LEADCHECKER_SETTINGS_STEP_SUBMIT_BACK"] = "Back to list";
